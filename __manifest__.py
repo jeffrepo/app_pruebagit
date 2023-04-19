5687471948c2337fea049c0d9e3455cce360d8bf
jeffrepo <jeffer.leo93@gmail.com>
2023-04-19 15:51:09 -0600

    PRUEBA APP GIT

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'APP PRUEBA GIT 2',
+    'version': '1.0',
+    'category': 'Inventory/Purchase',
+    'sequence': 100,
+    'summary': 'Será para una app de prueba DE GIT',
+    'website': '',
+    'depends': [],
+    'data': [
+    ],
+
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
