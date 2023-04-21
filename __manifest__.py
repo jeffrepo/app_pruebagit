09b5333f64b9f04baeca1a971698824faa7153fb
jeffrepo <jeffer.leo93@gmail.com>
2023-04-21 13:30:39 -0600

    v3.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'APP PRUEBA GIT 2',
-    'version': '2.0',
+    'version': '3.0',
     'category': 'Inventory/Purchase',
     'sequence': 100,
     'summary': 'Será para una app de prueba DE GIT',
