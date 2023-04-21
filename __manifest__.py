b5ebe778e7abb62c14a07c4dd407dca943132bdc
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2023-04-21 14:10:19 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'APP PRUEBA GIT 2',
-    'version': '3.0',
+    'version': '5.0',
     'category': 'Inventory/Purchase',
     'sequence': 100,
     'summary': 'Será para una app de prueba DE GIT',
